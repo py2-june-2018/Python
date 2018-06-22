7eb11167cd1d6d3a0938df4ccc5d657e7ac812c5
Kyle Kim <kmkimis6@gmail.com>
2018-06-21 22:55:22 -0700

    migration edit

--- dojo_ninjas/apps/ninjas/models.py
+++ dojo_ninjas/apps/ninjas/models.py
@@ -7,7 +7,7 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
-    desc = models.CharField(text_field=300)
+    desc = models.TextField(default="SOME STRING")
 
 class Ninja(models.Model):
     dojo_id = models.ForeignKey(Dojo)
